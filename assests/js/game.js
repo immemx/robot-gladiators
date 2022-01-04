f0991103633a04aa3cd79e4d087059fbb2886361
immemx <eayresm4@gmail.com>
2022-01-04 14:24:46 -0500

    loops added for other bad guys

--- assests/js/game.js
+++ assests/js/game.js
@@ -13,9 +13,14 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 var fight = function(enemyName) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+
+    while(enemyHealth > 0) {
+
+    //var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 
     if (promptFight === "fight" || promptFight === "FIGHT" ){
 
@@ -62,9 +67,11 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!")
     }
-
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i])
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName)
 };
