9920aafdd8f282b099e905082ccdf65e41212056
immemx <eayresm4@gmail.com>
2022-01-06 14:53:43 -0500

    must now have a valid name

--- assests/js/game.js
+++ assests/js/game.js
@@ -74,9 +74,20 @@ var randomNumber = function(min, max) {
 }
 
 
+var getPlayerName = function() {
+    var name = '';
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 // Player OBJECT
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
